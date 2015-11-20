16814ec9db99bd6f3997b333825a6b16870a4e13
Wirianto Djunaidi <wirianto_djunaidi@westpac.co.nz>
2015-11-20 14:58:03 +1300

    initial commit after adding router.js.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Notes
+
+Quick tutorial on React and Webpack from https://underthehood.myob.com/changing-of-the-guard-in-web-technologies.
+
+Adjust the content to follow Babel 6 convention.
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var webpack = require('webpack'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  path = require('path'),
+  srcPath = path.join(__dirname, 'src');
+
+module.exports = {
+  target: 'web',
+  cache: true,
+  entry: {
+    module: path.join(srcPath, 'module.js'),
+    common: ['react', 'react-router', 'alt']
+  },
+  resolve: {
+    root: srcPath,
+    extentions: ['', '.js'],
+    moduleDirectories: ['node_modules', 'src']
+  },
+  output: {
+    path: path.join(__dirname, 'tmp'),
+    publicPath: '',
+    filename: '[name].js',
+    library: ['Example', '[name]'],
+    pathInfo: true
+  },
+
+  module: {
+    loaders: [
+      { 
+        test: /\.jsx?$/, 
+        exclude: /node_modules/, 
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/index.html'
+    }),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  debug: true,
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    contentBase: './tmp',
+    historyApiFallback: true
+  }
+};
